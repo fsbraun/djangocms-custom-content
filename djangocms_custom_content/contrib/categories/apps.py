223d3dfa777819c222ce0fd19c0bddd8e353d18e
Fabian Braun <fsbraun@gmx.de>
2026-08-26 13:31:14 +0200

    chore: Rename contrib apps

--- djangocms_custom_content/contrib/categories/apps.py
+++ djangocms_custom_content/contrib/categories/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class CategoriesConfig(AppConfig):
     name = "djangocms_custom_content.contrib.categories"
     label = "djangocms_custom_content_categories"
-    verbose_name = _("Custom Content - Categories (Example)")
+    verbose_name = _("Global categories")
     default_auto_field = "django.db.models.BigAutoField"
--- djangocms_custom_content/contrib/services/apps.py
+++ djangocms_custom_content/contrib/services/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class ServicesConfig(AppConfig):
     name = "djangocms_custom_content.contrib.services"
     label = "djangocms_custom_content_services"
-    verbose_name = _("Custom Content - Services (Example)")
+    verbose_name = _("Services")
     default_auto_field = "django.db.models.BigAutoField"
